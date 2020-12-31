3069f8d8937043e0801a0478a83bcbf98a56f3ab
Emmanuel Durand <shernua@gmail.com>
2020-12-31 16:49:43 -0600

    handlebars

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,7 @@ var connection = require("./connection.js");
 var orm = {
     // Returns all 
     selectAll: function(table, cb) {
-        var queryString = `SELECT * FROM ${table} ;`;
+        var queryString = "SELECT * FROM " + table + ";";
         connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
