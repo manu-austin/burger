f37e17da8f4bce92c3fa59ec19be328130761046
Emmanuel Durand <shernua@gmail.com>
2020-12-31 14:09:34 -0600

    remove node modules + db + config

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, creationdate)
+VALUES
+(null, "McRoyal", FALSE, null),
+(null, "McBacon", FALSE, null),
+(null, "McFish", FALSE, null),
+(null, "McDoubleBacon", FALSE, null),
+(null, "McVegan", FALSE, null),
+(null, "McDeluxe", FALSE, null);
